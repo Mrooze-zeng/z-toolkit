cacdd381f6f9aea0f7ebe126be3eb76dfebe955e
John <mrooze@v-hiker.cn>
2021-12-02 22:38:40 +0800

    add extract readme plugin

---
+++ webpack/extract-readme-plugin.js
@@ -0,0 +1,52 @@
+const { RawSource } = require("webpack-sources");
+
+module.exports = class ExtractReadmePlugin {
+  constructor(
+    options = {
+      reg: /@readme/i,
+      packageNameReg: /\/packages\/([\w|-]+)\/src$/i,
+    },
+  ) {
+    this.options = options;
+    this.allExtractedComments = new Map();
+  }
+  apply(compiler) {
+    const pluginName = this.constructor.name;
+    compiler.hooks.compilation.tap(
+      pluginName,
+      (compilation, { normalModuleFactory }) => {
+        normalModuleFactory.hooks.parser
+          .for("javascript/auto")
+          .tap(pluginName, (parser) => {
+            parser.hooks.program.tap(pluginName, (ast, comments) => {
+              comments.forEach((comment) => {
+                if (this.options.reg.test(comment.value)) {
+                  const packageName = this.getPackageName(
+                    parser.state.current.context,
+                  );
+                  packageName &&
+                    (compilation.assets[`${packageName}.readme.md`] =
+                      new RawSource(
+                        this.getExtractComments(packageName, comment.value),
+                      ));
+                }
+              });
+            });
+          });
+      },
+    );
+  }
+  getPackageName(path = "") {
+    const match = path.match(this.options.packageNameReg);
+    return (match && match[1]) || "";
+  }
+  getExtractComments(packageName = "", comment = "") {
+    this.allExtractedComments.set(
+      packageName,
+      (this.allExtractedComments.has(packageName)
+        ? this.allExtractedComments.get(packageName)
+        : "") + comment.replace(this.options.reg, ""),
+    );
+    return this.allExtractedComments.get(packageName) || "";
+  }
+};
